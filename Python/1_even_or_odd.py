3cbaa9d81147eb2fabf4935a5a85f73c35682282
José Frank Hernández <josefrank@users.noreply.github.com>
2021-12-04 16:33:03 -0400

    Create 1_even_or_odd.py

---
+++ Python/1_even_or_odd.py
@@ -0,0 +1,14 @@
+"""
+Description:
+
+Create a function that takes an integer as an argument and returns
+"Even" for even numbers or "Odd" for odd numbers.
+"""
+
+# Solution
+
+def even_or_odd(number):
+    # Returns even if so, odd otherwise.
+    if number % 2 == 0:
+        return "Even"
+    return "Odd"
